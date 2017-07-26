c0dea55d0eef8fe5b0c632c4abf743cb0f7a6ada
Zvika Meiseles <Zvika.Meiseles@gmail.com>
2017-07-26 09:00:43 +0300

    Prog3 partial writeup (cont.)

--- Prog/Prog3/README.md
+++ Prog/Prog3/README.md
@@ -10,10 +10,6 @@ This article and code do not provide a complete solution to the challange.
 
 I never actually figured out the logic behind the maze's cheating. But I think the code provided is stil interesting.
 
-At first I thought my algorithm wasn't smart enough so the server is "punishing" me, so I tried to add tests and heuristics to predict wall locations and avoid dead-ends, but that turned out to be ... a dead-end :-)
-
-I also thought the cheats were time related (either "time between moves" or "time since start of execution"), but I could not find the logic there as well.
-
 
 ## Navigating the maze
 
@@ -36,40 +32,164 @@ MAZE_SERVER = ('34.211.117.64', 16000)
 RECV_SIZE = 8192
 
 def main():
-    conn = socket.create_connection(MAZE_SERVER)
-    response = conn.recv(RECV_SIZE)
+	conn = socket.create_connection(MAZE_SERVER)
+	response = conn.recv(RECV_SIZE)
 
-    complete = False
-    cont = False
+	complete = False
+	cont = False
 
-    while True:
-        print response
-        complete = False
-        # Do a thing here.
-        response_lines = response.splitlines()
-        if cont and not complete:
-            complete = True
-            cont = False
-        
-        if response_lines[0].startswith('Invalid movement'):
-            return
-        
-        if response_lines[0].startswith('-----------------------------------------'):
-            wall = True
+	while True:
+		print response
+		complete = False
+		# Do a thing here.
+		response_lines = response.splitlines()
+		if cont and not complete:
+			complete = True
+			cont = False
+		
+		if response_lines[0].startswith('Invalid movement'):
+			return
+		
+		if response_lines[0].startswith('-----------------------------------------'):
+			wall = True
 
-        if response_lines[len(response_lines)-1].startswith('-----------------------------------------') or response_lines[len(response_lines)-1].startswith('The possible moves are'):
-            complete = True
+		if response_lines[len(response_lines)-1].startswith('-----------------------------------------') or response_lines[len(response_lines)-1].startswith('The possible moves are'):
+			complete = True
 
-        if not complete:
-            cont = True
-            print "damn socket"
-        else:
-            conn.send(move)
+		if not complete:
+			cont = True
+			print "damn socket"
+		else:
+			conn.send(move)
 
-        response = conn.recv(RECV_SIZE)
+		response = conn.recv(RECV_SIZE)
 
 if __name__ == '__main__':
-    main()
+	main()
 ```
+Let's define a few auxiliary variables that will help us while we navigate the maze
+```python
+    # location changes when moving in each direction [ X-diff, Y-diff ]
+	directions = [ [0,-1], [1,0], [0,1], [-1,0] ]
+	# direction names in human-readable format
+	dirnames = [ '^', '>', 'V', '<' ]
+	# initial direction is RIGHT ('>')
+	direction = 1
+	# initial location
+	location = [ 1, 1 ]
+```
+We define our maze anarbitrary initial size of 64x64, where the upper left corner is (0,0) and X grows to the right (and Y grows 'down), so we start at (1,1).
+Each cell is a char representing its content:
+* '#' = wall
+* '.' = corridor, unvisited
+* 'o' = corridor, visited
+* 'X' = dead-end
+* '^', '>', 'V', '<' = player
+```python
+	w = 64
+	h = 64
+	maze = [['.' for x in range(w)] for y in range(h)]
+	maze[location[1]][location[0]] = dirnames[direction]
+```
+We also keep a "shadow" copy of the maze with a counter of how many times we visited each cell. This helps us decide which direction to go when faced with multiple corridors.
+```python
+	visits = [[0 for x in range(w)] for y in range(h)]
+	visits[location[1]][location[0]] = 1
+```
+Next, we'll check the response the server gave us and update our 2D map accordingly.
+```python
+for x in response_lines:
+    if x.startswith('Ahead you see'):
+        wall = False
+        if x.startswith('Ahead you see a hallway'):
+            d = direction
+            newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+            d = (direction + 1) % len(directions)
+            maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+            d = (direction + len(directions) - 1) % len(directions)
+            maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+            pass
+        elif x.startswith('Ahead you see some turns'):
+            pass
+        elif x.startswith('Ahead you see a right turn'):
+            d = direction
+            newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+            d = (direction + len(directions) - 1) % len(directions)
+            maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+            pass
+        elif x.startswith('Ahead you see a left turn'):
+            d = direction
+            newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+            d = (direction + 1) % len(directions)
+            maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+            pass
+        elif x.startswith('Ahead you see a dead end'):
+            d = direction
+            maze[location[1]+directions[d][1]][location[0]+directions[d][0]] = 'X'
+            newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+            maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+            d = (direction + 1) % len(directions)
+            maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+            d = (direction + len(directions) - 1) % len(directions)
+            maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+            pass
+    elif x.startswith('Invalid movement'):
+        return
+```
+Note that in order to turn right, we need to `+1` our `direction` variable, while in order to turn left we `-1`. Since we need to keep the direction inside the range `[0,3]`, we notice 2 things:
+1. we must use the modulo operator `%` to wrap around the boundary
+2. as a result, `-1` is identical to `+3`
 
-`TODO: complete writeup`
+So turning *right* becomes `new_direction = (direction + 1) % len(directions)` while turning *left* is `new_direction = (direction + len(directions) - 1) % len(directions)`.
+
+After we've updated the maze, we need to figure out where we should go next. We check all the cells around our current location, and grade them according to the information we have. We then choose the cell with the lowest score.
+```python
+    grade = []
+    for i in [0, 1, 3, 2]:
+        # find out where we've never been and go there
+        d = (direction + i) % len(directions)
+        c = maze[location[1]+directions[d][1]][location[0]+directions[d][0]];
+        v = visits[location[1]+directions[d][1]][location[0]+directions[d][0]];
+        g = { '#': 9999, 'o': v, '.': 0, 'X': 9998}[c]
+        grade.append({'dir': i, 'grade': g})
+    m = min(grade, key=lambda k: k['grade'])
+    if m['grade'] == 9999:
+        print "STUCK!"
+        return
+    moves.append(valid_moves[m['dir']])
+```
+In case we're sorrounded by walls (because the game cheats!!!), we just give up.
+**NOTE**
+This is where we should have added some logic to detect the impending "cheat" and avoid it.
+
+The last step is to act according to the selected step
+```python
+    # mark current location as visited
+    maze[location[1]][location[0]] = 'o'
+    # increment visits counter
+    visits[location[1]][location[0]] += 1
+    if move == 'w':
+        # walk forward, but don't walk into a wall
+        if not wall:
+            location[0] += directions[direction][0]
+            location[1] += directions[direction][1]
+    if move == 's':
+        # walk backwards
+        visits[location[1]][location[0]] += 1
+        location[0] -= directions[direction][0]
+        location[1] -= directions[direction][1]
+    elif move == 'd':
+        # turn right
+        direction = (direction + 1) % len(directions)
+    elif move == 'a':
+        # turn left
+        direction = (direction + len(directions) - 1) % len(directions)
+    
+    # update player's 'icon' to match facing direction
+    maze[location[1]][location[0]] = dirnames[direction]
+    
+	allmoves.append(move)
+	conn.send(move)
+	move_count += 1
+```
+... and that's as far as I got.
--- Prog/Prog3/maze_client.py
+++ Prog/Prog3/maze_client.py
@@ -12,170 +12,166 @@ valid_moves = ['w', 'd', 'a', 'a' ]
 cheats = []
 
 def display(maze):
-	w = len(maze[0])
-	h = len(maze)
-	for y in range(h):
-		for x in range(w):
-			sys.stdout.write('%s' % maze[y][x])
-		sys.stdout.write('\n')
-	sys.stdout.write('\n')
+    w = len(maze[0])
+    h = len(maze)
+    for y in range(h):
+        for x in range(w):
+            sys.stdout.write('%s' % maze[y][x])
+        sys.stdout.write('\n')
+    sys.stdout.write('\n')
 
 def main():
-	global cheats
-	random.seed()
-	conn = socket.create_connection(MAZE_SERVER)
-	response = conn.recv(RECV_SIZE)
+    global cheats
+    random.seed()
+    conn = socket.create_connection(MAZE_SERVER)
+    response = conn.recv(RECV_SIZE)
 
-	allmoves = []
-	moves = []
-	complete = False
-	directions = [ [0,-1], [1,0], [0,1], [-1,0] ]
-	dirnames = [ '^', '>', 'V', '<' ]
-	direction = 1
-	location = [ 1, 1 ]
-	wall = False
-	cont = False
-	same_loc = 0
-	move_count = 0
-	
-	w = 64
-	h = 32
-	maze = [['.' for x in range(w)] for y in range(h)]
-	maze[location[1]][location[0]] = dirnames[direction]
-	visits = [[0 for x in range(w)] for y in range(h)]
-	visits[location[1]][location[0]] = 1
-	
-	start = datetime.datetime.now()
-	while True:
-		#now = datetime.datetime.now()
-		print response
-		#print time.asctime()
-		complete = False
-		# Do a thing here.
-		response_lines = response.splitlines()
-		if cont and not complete:
-			complete = True
-			cont = False
-		
-		if response_lines[0].startswith('Invalid movement'):
-			return
-		
-		if response_lines[0].startswith('-----------------------------------------'):
-			wall = True
-			# if there wasn't a wall there before... it's cheating
-			if maze[location[1]+directions[direction][1]][location[0]+directions[direction][0]] == 'o':
-				cheats.append({'time': now, 'relative_time': now-start, 'moves': move_count, 'location': [location[1]+directions[direction][1], location[0]+directions[direction][0]] })
-				pass
-			maze[location[1]+directions[direction][1]][location[0]+directions[direction][0]] = '#'
+    allmoves = []
+    moves = []
+    complete = False
+    directions = [ [0,-1], [1,0], [0,1], [-1,0] ]
+    dirnames = [ '^', '>', 'V', '<' ]
+    direction = 1
+    location = [ 1, 1 ]
+    wall = False
+    cont = False
+    move_count = 0
+    
+    w = 64
+    h = 32
+    maze = [['.' for x in range(w)] for y in range(h)]
+    maze[location[1]][location[0]] = dirnames[direction]
+    visits = [[0 for x in range(w)] for y in range(h)]
+    visits[location[1]][location[0]] = 1
+    
+    start = datetime.datetime.now()
+    while True:
+        #now = datetime.datetime.now()
+        print response
+        #print time.asctime()
+        complete = False
+        # Do a thing here.
+        response_lines = response.splitlines()
+        if cont and not complete:
+            complete = True
+            cont = False
+        
+        if response_lines[0].startswith('Invalid movement'):
+            return
+        
+        if response_lines[0].startswith('-----------------------------------------'):
+            wall = True
+            # if there wasn't a wall there before... it's cheating
+            if maze[location[1]+directions[direction][1]][location[0]+directions[direction][0]] == 'o':
+                cheats.append({'time': now, 'relative_time': now-start, 'moves': move_count, 'location': [location[1]+directions[direction][1], location[0]+directions[direction][0]] })
+                pass
+            maze[location[1]+directions[direction][1]][location[0]+directions[direction][0]] = '#'
 
-		if response_lines[len(response_lines)-1].startswith('-----------------------------------------') or response_lines[len(response_lines)-1].startswith('The possible moves are') or len(response_lines) == 22:
-			complete = True
+        if response_lines[len(response_lines)-1].startswith('-----------------------------------------') or response_lines[len(response_lines)-1].startswith('The possible moves are') or len(response_lines) == 22:
+            complete = True
 
-		if not complete:
-			cont = True
-			print "damn socket"
-		else:
-			for x in response_lines:
-				if x.startswith('Ahead you see'):
-					wall = False
-					if x.startswith('Ahead you see a hallway'):
-						d = direction
-						newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
-						d = (direction + 1) % len(directions)
-						maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
-						d = (direction + len(directions) - 1) % len(directions)
-						maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
-						pass
-					elif x.startswith('Ahead you see some turns'):
-						pass
-					elif x.startswith('Ahead you see a right turn'):
-						d = direction
-						newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
-						d = (direction + len(directions) - 1) % len(directions)
-						maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
-						pass
-					elif x.startswith('Ahead you see a left turn'):
-						d = direction
-						newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
-						d = (direction + 1) % len(directions)
-						maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
-						pass
-					elif x.startswith('Ahead you see a dead end'):
-						d = direction
-						maze[location[1]+directions[d][1]][location[0]+directions[d][0]] = 'X'
-						newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
-						maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
-						d = (direction + 1) % len(directions)
-						maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
-						d = (direction + len(directions) - 1) % len(directions)
-						maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
-						pass
-				elif x.startswith('Invalid movement'):
-					return
-			
-			display(maze)
-			if len(moves) == 0:
-				grade = []
-				for i in [0, 1, 3, 2]:
-					# find out where we've never been and go there
-					d = (direction + i) % len(directions)
-					c = maze[location[1]+directions[d][1]][location[0]+directions[d][0]];
-					v = visits[location[1]+directions[d][1]][location[0]+directions[d][0]];
-					g = { '#': 9999, 'o': v, '.': 0, 'X': 9998}[c]
-					grade.append({'dir': i, 'grade': g})
-				m = min(grade, key=lambda k: k['grade'])
-				print grade, m
-				#time.sleep(1)
-				if m['grade'] == 9999:
-					print "STUCK!"
-					return
-				moves.append(valid_moves[m['dir']])
-				
-			#print moves
-			if len(moves) > 0:
-				move = moves.pop()
-			else:
-				#move = valid_moves[random.randint(0, 3)]
-				#move = raw_input("Your Move: ")
-				pass
-			if not len(move):
-				return
-			
-			maze[location[1]][location[0]] = 'o'
-			visits[location[1]][location[0]] += 1
-			if move == 'w':
-				if not wall:
-					same_loc = 0
-					location[0] += directions[direction][0]
-					location[1] += directions[direction][1]
-				else:
-					same_loc += 1
-			if move == 's':
-				same_loc = 0
-				visits[location[1]][location[0]] += 1
-				location[0] -= directions[direction][0]
-				location[1] -= directions[direction][1]
-			elif move == 'd':
-				same_loc += 1
-				direction = (direction + 1) % len(directions)
-			elif move == 'a':
-				same_loc += 1
-				direction = (direction + len(directions) - 1) % len(directions)
-			
-			maze[location[1]][location[0]] = dirnames[direction]
-			
-			if same_loc > 8:
-				print "STUCK!"
-				move = 'q'
-			
-			allmoves.append(move)
-			#print allmoves
-			conn.send(move)
-			move_count += 1
-		response = conn.recv(RECV_SIZE)
+        if not complete:
+            cont = True
+            print "damn socket"
+        else:
+            for x in response_lines:
+                if x.startswith('Ahead you see'):
+                    wall = False
+                    if x.startswith('Ahead you see a hallway'):
+                        d = direction
+                        newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+                        d = (direction + 1) % len(directions)
+                        maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+                        d = (direction + len(directions) - 1) % len(directions)
+                        maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+                        pass
+                    elif x.startswith('Ahead you see some turns'):
+                        pass
+                    elif x.startswith('Ahead you see a right turn'):
+                        d = direction
+                        newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+                        d = (direction + len(directions) - 1) % len(directions)
+                        maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+                        pass
+                    elif x.startswith('Ahead you see a left turn'):
+                        d = direction
+                        newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+                        d = (direction + 1) % len(directions)
+                        maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+                        pass
+                    elif x.startswith('Ahead you see a dead end'):
+                        d = direction
+                        maze[location[1]+directions[d][1]][location[0]+directions[d][0]] = 'X'
+                        newloc = [location[0]+directions[d][0],location[1]+directions[d][1]]
+                        maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+                        d = (direction + 1) % len(directions)
+                        maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+                        d = (direction + len(directions) - 1) % len(directions)
+                        maze[newloc[1]+directions[d][1]][newloc[0]+directions[d][0]] = '#'
+                        pass
+                elif x.startswith('Invalid movement'):
+                    return
+            
+            display(maze)
+            if len(moves) == 0:
+                grade = []
+                for i in [0, 1, 3, 2]:
+                    # find out where we've never been and go there
+                    d = (direction + i) % len(directions)
+                    c = maze[location[1]+directions[d][1]][location[0]+directions[d][0]];
+                    v = visits[location[1]+directions[d][1]][location[0]+directions[d][0]];
+                    g = { '#': 9999, 'o': v, '.': 0, 'X': 9998}[c]
+                    grade.append({'dir': i, 'grade': g})
+                m = min(grade, key=lambda k: k['grade'])
+                print grade, m
+                #time.sleep(1)
+                if m['grade'] == 9999:
+                    print "STUCK!"
+                    return
+                moves.append(valid_moves[m['dir']])
+                
+            #print moves
+            if len(moves) > 0:
+                move = moves.pop()
+            else:
+                #move = valid_moves[random.randint(0, 3)]
+                #move = raw_input("Your Move: ")
+                pass
+            if not len(move):
+                return
+            
+            # mark current location as visited
+            maze[location[1]][location[0]] = 'o'
+            # increment visits counter
+            visits[location[1]][location[0]] += 1
+            if move == 'w':
+                # walk forward, but don't walk into a wall
+                if not wall:
+                    location[0] += directions[direction][0]
+                    location[1] += directions[direction][1]
+            if move == 's':
+                # walk backwards
+                visits[location[1]][location[0]] += 1
+                location[0] -= directions[direction][0]
+                location[1] -= directions[direction][1]
+            elif move == 'd':
+                # turn right
+                direction = (direction + 1) % len(directions)
+            elif move == 'a':
+                # turn left
+                direction = (direction + len(directions) - 1) % len(directions)
+
+            # update player's 'icon' to match facing direction
+            maze[location[1]][location[0]] = dirnames[direction]
+            
+            allmoves.append(move)
+            #print allmoves
+            conn.send(move)
+            move_count += 1
+        response = conn.recv(RECV_SIZE)
 
 if __name__ == '__main__':
-	main()
-	#pp = pprint.PrettyPrinter(indent=4)
-	#pp.pprint(cheats)
-	
+    main()
+    #pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(cheats)
+    
